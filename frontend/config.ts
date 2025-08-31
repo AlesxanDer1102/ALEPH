fd3f50078a7b6e2b69cff04aa7d1fa15472497c6
AlesxanDer1102 <diego.orrego.t@uni.pe>
2025-08-30 22:33:51 -0500

    feat: actualizar configuración de Alchemy para usar variables de entorno

--- frontend/config.ts
+++ frontend/config.ts
@@ -8,14 +8,14 @@ import { env } from "process"
 export const externalWalletsConfig = configForExternalWallets({
     wallets: ["wallet_connect", "metamask"],
     chainType: ["evm"],
-    walletConnectProjectId: "4ef621c5cc218c4e769d39a1b2f14b8a",
+    walletConnectProjectId: env.NEXT_PUBLIC_PROJECT_ID || "",
 })
 
 export const config = createConfig(
     {
         transport: alchemy({
             // Replace with your API key
-            apiKey: "s68ftd4vS3JDby3xS71_M4sU-KtxRLWJ",
+            apiKey: env.NEXT_PUBLIC_ALCHEMY_AA_API_KEY || "",
         }),
         chain: sepolia,
         ssr: true,
@@ -23,7 +23,7 @@ export const config = createConfig(
         enablePopupOauth: true,
         // For gas sponsorship (optional)
         // Learn more here: https://www.alchemy.com/docs/wallets/transactions/sponsor-gas/sponsor-gas-evm
-        policyId: "4a2d2b13-03a3-4e35-b9de-a1375b3e31fe",
+        policyId: env.NEXT_PUBLIC_ALCHEMY_POLICY_ID || "",
     },
     {
         auth: {
